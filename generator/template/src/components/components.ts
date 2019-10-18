fcc906fee0d192f7c400f440e81e57deb8d25dcc
lixiaowei <promise9495@163.com>
2019-10-18 11:00:27 +0800

    修改组件自动注册在生产环境下name获取失败

--- generator/template/src/components/components.ts
+++ generator/template/src/components/components.ts
@@ -1,9 +1,12 @@
 export default (Vue: any) => {
   const requireComponent = require.context("@/components", true, /\.(vue)$/);
 
-  requireComponent.keys().forEach(fileName => {
+  requireComponent.keys().forEach((fileName: string) => {
     const componentConfig = requireComponent(fileName);
-    const componentName = componentConfig.default.name;
+    const componentName = fileName.substring(
+      fileName.lastIndexOf("/") + 1,
+      fileName.search(/(\.vue|\.js)/)
+    );
     Vue.component(componentName, componentConfig.default || componentConfig);
   });
 };
